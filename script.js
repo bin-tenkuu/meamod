261342d071d316fc214229f6687ec1758d5ce10b
ccr <2938137849@qq.com>
2019-06-06 09:26:25 +0800

    整合字体mod

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ requestAnimationFrame(function autoRun() {
 	try {
 		const arrBackup = cfg.voice.sound.groups_
 		if (!arrBackup || arrBackup.length === 0) {
-			throw new Error()
+			throw new Error();
 		}
 		console.log('Hacked语音文本');
 		Object.entries(cfg.voice.sound.groups_).forEach(([soundID, soundGroup]) => {
@@ -113,3 +113,70 @@ requestAnimationFrame(function autoRun() {
 		raf = requestAnimationFrame(autoRun);
 	}
 })
+
+(function () {
+  const fontMap = {
+    fengyu: {
+      src: 'url(/fengyu.ttf)',
+    },
+    hanyi: {
+      src: 'url(/hanyi.ttf)',
+    },
+    haolong: {
+      src: 'url(/haolong.otf)',
+    },
+    youyuan: {
+      src: 'local("幼圆")',
+    },
+    youyuan_bold: {
+      src: 'local("幼圆")',
+      bold: true,
+    },
+  }
+  {
+    let css = ''
+    for (let [name, info] of Object.entries(fontMap)) {
+      css += `@font-face{font-family:"${name}";src:${info.src};${info.bold ? 'font-weight:bold;' : ''}}`
+    }
+    const style = document.createElement('style')
+    style.id = '8q_font_override'
+    style.appendChild(document.createTextNode(css))
+    document.head.appendChild(style)
+  }
+  const registerBitmapFont = Laya.Text.registerBitmapFont
+  Laya.Text.registerBitmapFont = function(name, font) {
+    if (fontMap.hasOwnProperty(name)) {
+      font.destroy()
+      font.fontName_8q = name === 'hanyi' ? 'hanyi,haolong' : name
+    } else {
+      console.log('缺少字体定义:', font, name)
+    }
+    return registerBitmapFont.call(this, name, font)
+  }
+  const ctx = document.createElement('canvas').getContext("2d");
+  const getCharWidth = Laya.BitmapFont.prototype.getCharWidth
+  Laya.BitmapFont.prototype.getCharWidth = function(chr) {
+    if (this.fontName_8q) {
+      ctx.font = '40px ' + this.fontName_8q
+      return ctx.measureText(chr).width + this.letterSpacing
+    } else {
+      return getCharWidth.call(this, chr)
+    }
+  }
+  const drawText = Laya.BitmapFont.prototype.drawText
+  Laya.BitmapFont.prototype.drawText = function(str, textObj, x, y, align, width) {
+    if (this.fontName_8q) {
+      const font = '40px ' + this.fontName_8q
+      let textWidth = this.getTextWidth(str),
+        left = 0
+      if ('center' === align) left = (width - textWidth) / 2
+      else if ('right' === align) left = width - textWidth
+      for (let index = 0, len = str.length; index < len; index++) {
+        textObj.graphics.fillText(str.charAt(index), x + left, y, font, '', 'left')
+        left += this.getCharWidth(str.charAt(index))
+      }
+    } else {
+      return drawText.call(this, str, textObj, x, y, align, width)
+    }
+  }
+}())
